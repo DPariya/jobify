0cf389a1752eab2bf52b9b845b1f703f752ebf96
Dipti Pariya <diptipariya@gmail.com>
2025-07-09 15:14:32 +0530

    refactor: server file, missing patch, job modal missing timestamp

--- jobify-backend/models/Job.js
+++ jobify-backend/models/Job.js
@@ -4,38 +4,41 @@
 
 import { mongoose } from 'mongoose';
 
-const jobSchema = new mongoose.Schema({
-  position: {
-    type: String,
-    required: [true, 'Position is required'],
-    trim: true,
+const jobSchema = new mongoose.Schema(
+  {
+    position: {
+      type: String,
+      required: [true, 'Position is required'],
+      trim: true,
+    },
+    company: {
+      type: String,
+      required: [true, 'Company is required'],
+      trim: true,
+    },
+    jobLocation: {
+      type: String,
+      default: 'my city',
+      trim: true,
+    },
+    jobStatus: {
+      type: String,
+      enum: ['pending', 'interview', 'declined', 'offer'],
+      default: 'pending',
+    },
+    jobType: {
+      type: String,
+      enum: ['full-time', 'part-time', 'remote', 'contract', 'internship'],
+      default: 'full-time',
+    },
+    createdBy: {
+      type: mongoose.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
   },
-  company: {
-    type: String,
-    required: [true, 'Company is required'],
-    trim: true,
-  },
-  jobLocation: {
-    type: String,
-    default: 'my city',
-    trim: true,
-  },
-  jobStatus: {
-    type: String,
-    enum: ['pending', 'interview', 'declined', 'offer'],
-    default: 'pending',
-  },
-  jobType: {
-    type: String,
-    enum: ['full-time', 'part-time', 'remote', 'contract', 'internship'],
-    default: 'full-time',
-  },
-  createdBy: {
-    type: mongoose.Types.ObjectId,
-    required: true,
-    ref: 'User',
-  },
-});
+  { timestamps: true }
+);
 
 jobSchema.index({ createdBy: 1 }); // fast filtering by user
 jobSchema.index({ jobStatus: 1 }); // fast status filter
